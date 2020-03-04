987cae4021ec86ab005de0291d366880b168b5be
Haastrup Elijah <haastrupea@gmail.com>
2020-03-04 07:43:14 +0100

    add 1.errorpage route 2.request form route 3. clean up the code

--- app/mainland.php
+++ app/mainland.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
+include_once 'lib/loader.php';
 include_once 'route/Router.php';
 include_once 'model/loader.php';
 include_once 'controller/loader.php';
 include_once 'view/loader.php';
 
 $router= new Router;
-
 
 $router->get("/[home]",function(){
     //load the controller
@@ -16,31 +16,30 @@ $router->get("/[home]",function(){
 
 
 //POST REQUEST
-$router->post("/home-tab",function($req,$arg){
+$router->post("/home-tab",function($req){
     
     
     //redirect to landing page with form of search type preloaded
     $post=$req->getBody();
     $search=filter_var($post['search_base_on'],FILTER_SANITIZE_SPECIAL_CHARS);
     header("Location: /home/{$search}");
-    
-
-    //helps with toggling of price, location and property Types tab on the home page
-    // $method=(isset($_POST['search_base_on']))?$method=$_POST['search_base_on']:"";
-    // $searchType=new landingPageController();
-    // $searchType->run($method);
 });
 
 
 $router->get("/house-search/{searchType}/{searchTerm}",function($req,$arg){
 
     $action=$arg['searchType']; $param=$arg['searchTerm'];
-
+    
     //loadController
     $resultPage=new resultPageController();
     $resultPage->run($action,$param);
 });
 
+$router->get("/house/images/{view}/{quality}/{house_id}",function($req,$arg){
+    //view=front/back/bath/dinning/right/left/kitchen/bed
+    //quality=thumbnail/mobile/desktop/hd;
+    //house_id=house id used
+});
 
 $router->get("/house-search/{searchType}",function($req,$arg){
     //redirect to landing page with form of search type preloaded
@@ -51,15 +50,16 @@ $router->get("/house-search/{searchType}",function($req,$arg){
 });
 
 
-$router->get("/house-search",function($req,$arg){
+$router->get("/house-search",function(){
     //to redirect general search back here
     if(isset($_SESSION['generalSearch'])){
         //aet both action and action
-        $param=$_SESSION['generalSearch']; $action='generalSearch';
+        $param=$_SESSION['generalSearch']; $action='searchForm';
+
 
             //loadController
-        $generalsearch=new resultPageController();
-        $generalsearch->run($action,$param);
+        $search=new resultPageController();
+        $search->run($action,$param);
 
         //unset the session
         unset($_SESSION['generalSearch']);
@@ -74,7 +74,7 @@ $router->get("/house-search",function($req,$arg){
 
 
 //POST REQUEST
-$router->post("/general-search",function($req,$arg){
+$router->post("/general-search",function($req){
     $post=$req->getBody();
 
     $_SESSION['generalSearch']=$post;
@@ -82,7 +82,7 @@ $router->post("/general-search",function($req,$arg){
     exit();
 });
 
-$router->post("/house-search",function($req,$arg){
+$router->post("/house-search",function($req){
     $post=$req->getBody();
 
     foreach ($post as $key => $value) {
@@ -99,7 +99,7 @@ $router->post("/house-search",function($req,$arg){
 $router->get("/home/{params}",function($req,$arg){
     //load the controller
     $method='';
-    switch ($arg['params']) {
+    switch (strtolower($arg['params'])) {
         case 'price':
             $method="priceForm";
             break;
@@ -117,4 +117,28 @@ $router->get("/home/{params}",function($req,$arg){
     $landingPage->run($method);
 });
 
+
+
+$router->get("/house/{action}/{id}",function($req,$arg){
+    //can be view detail or show request house form
+
+
+    $house_id=$arg['id'];
+    $action=$arg['action'];
+
+    $house=new houseInfoPageController;
+    $house->run($action,$house_id);
+
+});
+
+$router->post("/house/requested",function($req){
+
+    //post data
+    $formData=$req->getBody();
+    $action="requested";
+    $house=new houseInfoPageController;
+    $house->run($action,$formData);
+});
+
+$router->errorPage("/home");
 $router->run();
