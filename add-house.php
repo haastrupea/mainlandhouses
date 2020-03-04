d5a85b64798938dd1bfec574b3c7e906d9b8c2d6
Haastrup Elijah <haastrupea@gmail.com>
2020-03-04 07:55:51 +0100

    form to add house to list of houses in the data base

---
+++ add-house.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/template/styles/bs/bs.min.css">
+    <link rel="stylesheet" href="/template/styles/fa/css/all.min.css">
+    <title>Add house to database</title>
+</head>
+<body>
+    <div class="container-fluid">
+    <div class="row">
+    <form action="#">
+    <div class="form-group">
+    <div class="input-group">
+    
+    <input type="text" class="form-control">
+    </div>
+    </div>
+    </form>
+    </div>
+    </div>
+</body>
+</html>
